77df4d0b5aa27c7aae96c5ecf42d0d2d00dfd636
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-10-29 13:20:03 +0200

    Some docs

--- lib/rocket/helpers.rb
+++ lib/rocket/helpers.rb
@@ -1,6 +1,15 @@
 module Rocket
   module Helpers
     
+    # Hash given, returns it version with symbolized keys. 
+    #
+    #   p symbolize_keys("hello" => "world", ["Array here"] => "yup")
+    #   p symbolize_keys(:one => 1, "two" => 2)
+    # will produce:
+    #
+    #  {:hello => "world", ["Array here"] => "yup"}
+    #  {:one => 1, :two => 2}
+    #
     def symbolize_keys(hash)
       return hash unless hash.is_a?(Hash)
       hash.inject({}) do |options, (key, value)|
@@ -9,6 +18,7 @@ module Rocket
       end
     end
     
+    # Proxy to Rocket's configured logger. 
     def log
       Rocket.logger
     end
